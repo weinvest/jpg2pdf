cedb7ac7367c39a5e468fe43976dc45d46f4564f
weinvest <asd1831031@163.com>
2021-06-17 19:08:58 +0800

    Update jpg2pdf.py
    bug fix

--- jpg2pdf.py
+++ jpg2pdf.py
@@ -1,4 +1,3 @@
-#coding=utf-8
 #!/usr/bin/env python3
 
 """
@@ -6,14 +5,14 @@ convert image to pdf file
 """
 
 #Author: mrbeann <https://github.com/mrbeann/jpg2pdf>
-import os  
+import os
 import sys
-import glob 
-import platform 
+import glob
+import platform
 
-from reportlab.lib.pagesizes import letter, A4, landscape  
+from reportlab.lib.pagesizes import letter, A4, landscape
 from reportlab.platypus import SimpleDocTemplate
-from reportlab.lib.units import inch  
+from reportlab.lib.units import inch
 from reportlab.pdfgen import canvas
 from reportlab import rl_settings
 
@@ -33,15 +32,15 @@ def topdf(path,recursion=None,pictureType=None,sizeMode=None,width=None,height=N
                 None or False for no recursion
                 True for recursion to children folder
                 wether to recursion or not
-    
+
     pictureType : list
                   type of pictures,for example :jpg,png...
-    sizeMode : int 
+    sizeMode : int
            None or 0 for pdf's pagesize is the biggest of all the pictures
            1 for pdf's pagesize is the min of all the pictures
            2 for pdf's pagesize is the given value of width and height
            to choose how to determine the size of pdf
-    
+
     width : int
             width of the pdf page
 
@@ -53,8 +52,8 @@ def topdf(path,recursion=None,pictureType=None,sizeMode=None,width=None,height=N
            True for keep the size of the pictures
            wether to keep the picture size or not
 
-    save : string 
-           path to save the pdf 
+    save : string
+           path to save the pdf
     """
     print(path)
     if platform.system() == 'Windows':
@@ -68,11 +67,12 @@ def topdf(path,recursion=None,pictureType=None,sizeMode=None,width=None,height=N
                 topdf(path+i,recursion,pictureType,sizeMode,width,height,fit,save)
     filelist = []
     if pictureType == None:
-        filelist = glob.glob(os.path.join(path, '*.jpg')) 
+        filelist = glob.glob(os.path.join(path, '*.jpg'))
     else:
         for i in pictureType:
             filelist.extend(glob.glob(os.path.join(path, '*.'+i)))
 
+    filelist = list(sorted(filelist))
     maxw = 0
     maxh = 0
     if sizeMode == None or sizeMode == 0:
@@ -100,25 +100,28 @@ def topdf(path,recursion=None,pictureType=None,sizeMode=None,width=None,height=N
 
     maxsize = (maxw,maxh)
     if save == None:
-        filename_pdf = path + path.split('/')[-2]
+        filename_pdf = path
     else:
-        filename_pdf = save + path.split('/')[-2]
+        filename_pdf = save
+
+    filename_pdf = os.path.join(filename_pdf, os.path.basename(path if '/' != path[-1] else path[:-1]))
+
     #filename_pdf = filename_pdf.decode('utf8','ignore')
     filename_pdf = filename_pdf + '.pdf'
     print(filename_pdf)
     c = canvas.Canvas(filename_pdf, pagesize=maxsize )
-     
+
     l = len(filelist)
-    for i in range(l): 
+    for i in range(l):
         print(filelist[i])
         (w, h) =maxsize
-        width, height = letter 
+        width, height = letter
         if fit == True:
-            c.drawImage(filelist[i] , 0,0) 
+            c.drawImage(filelist[i] , 0,0)
         else:
-            c.drawImage(filelist[i] , 0,0,maxw,maxh) 
-        c.showPage()  
-    c.save()      
+            c.drawImage(filelist[i] , 0,0,maxw,maxh)
+        c.showPage()
+    c.save()
     print("end.")
 
 if __name__ == '__main__':
@@ -131,7 +134,6 @@ if __name__ == '__main__':
            2 the given value of width and height''')
     parser.add_argument('-W', '--width', type=int, default=0, help='only needed when size_mode = 2')
     parser.add_argument('-H', '--height', type=int, default=0, help='only needed when size_mode = 2')
-    args = parser.parse_args(sys.argv[1:]) 
+    args = parser.parse_args(sys.argv[1:])
     print(args.pict_types)
     topdf(args.source_dir, pictureType=args.pict_types, save=args.source_dir, sizeMode=args.size_mode, width=args.width, height=args.height)
-
